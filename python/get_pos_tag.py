3e3299e26b8b40eae0de3cbfdea72dbdfb486be4
BastienCarbonnier <bastien.carbonnier@gmail.com>
2019-04-29 17:23:33 +0200

    Tests TreeTagger et spaCy

--- python/get_pos_tag.py
+++ python/get_pos_tag.py
@@ -4,7 +4,7 @@ import json
 import spacy
 
 nlp = spacy.load("fr_core_news_sm")
-doc = nlp(sys.argv[1])
+doc = nlp("Comme un hôtel de 100 chambres, je dirais que c'ordinaire, bien que je ne suis pas un ami de grands hôtels. Nous avons apprécié d'espace de notre chambre et la vue sur la mer. Demandez une chambre avec vue sur la mer et la piscine de aux derniers étages (nous avions une suite). Personnel de ménage était excellent, même pour les enfants était petites choses rangée chaque jour :-) Nous avons aussi beaucoup aimé la piscine. Le petit déjeuner continental habituel, sauf Nous avons trouvé un peu de moisissure dans un melon un matin... Le personnel de la réception ne sont pas des plus chaleureux !")#sys.argv[1])
 
 list =[]
 i = 0
