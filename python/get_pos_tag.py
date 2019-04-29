c91e4a25bfde3641b2fe0f3e52750ed73e814c7c
BastienCarbonnier <bastien.carbonnier@gmail.com>
2019-04-30 01:20:10 +0200

    Ajout du lem dans la structure de donnée représentant la phrase

--- python/get_pos_tag.py
+++ python/get_pos_tag.py
@@ -64,7 +64,7 @@ def tagToObj(sentArray):
     sentTab = []
     for t in sentArray:
         if len(t) >1 :
-            sentTab.append({"mot" : t[0], "nature" : tags[t[1]],"tag":t[1],"acc":t[3]})
+            sentTab.append({"mot" : t[0], "nature" : tags[t[1]],"tag":t[1],"lem":t[2],"acc":t[3]})
     return sentTab
 
 def posTaggingTreeTagger(text) :
